bed6a7b83688798127abed39c678fcd55008aa58
Jamesllm <ronaljamesllapapascamontes@gmail.com>
2023-04-18 22:31:40 -0500

    db port

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ config();
 export default {
   host: process.env.HOST || "localhost",
   database: process.env.DATABASE || "",
+  db_port: process.env.DBPORT || "",
   user: process.env.USER || "root",
   password: process.env.PASSWORD || "",
 };
--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ import config from "../config/config";
 const connection = mysql.createConnection({
   host: config.host,
   database: config.database,
+  db_port: config.db_port,
   user: config.user,
   password: config.password,
 });
